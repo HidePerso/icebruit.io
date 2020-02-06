c395ab3ea5ed8be9464106325b80a3c412e7da14
HidePerso <hideperso@gmail.com>
2020-02-06 20:30:58 +0300

    Попытка добавить стилизацию

--- src/components/bruit-core/bruit-core.tsx
+++ src/components/bruit-core/bruit-core.tsx
@@ -481,18 +481,7 @@ export class BruitCore {
         style={{ 'background-color': this._bruitIoConfig.colors.header }}
       >
         <h1 class="bruit-title" part="bruit-title">{this._bruitIoConfig.labels.title}</h1>
-        <a id="bruit-io-btn-close">
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            fill="white"
-          >
-            <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
-          </svg>
-        </a>
+        <a id="bruit-io-btn-close" part="bruit-io-btn-close"></a>
       </div>
     );
   }
